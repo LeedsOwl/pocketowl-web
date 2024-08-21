7a37683c9eb2d2b28cae3caad119e9ab503d51a3
Karthikeyan Arumugam <kamtechie@gmail.com>
2024-08-21 21:13:53 +0100

    added flowbite-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "flowbite-react/components/Button";
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-red-300">
         My React and TypeScript App!
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
