bb84cbddb43bebc366b6bbf1be7c4f475c91257b
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2024-08-28 23:49:35 +0100

    adding svg to background to balance

--- src/components/ui/balance.tsx
+++ src/components/ui/balance.tsx
@@ -9,13 +9,20 @@ interface BalanceProps {
 
 const Balance: React.FC<BalanceProps> = ({ accountBalance, income, expenses }) => {
   return (
-    <div className="bg-gradient-to-t from-gray-600 to-gray-900 text-white p-6 rounded-lg shadow-md">
+    <div
+      className="text-white p-6 rounded-lg shadow-md"
+      style={{
+        backgroundImage: "url('/stacked-waves.svg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <div className="text-center mb-6">
         <p className="text-lg">Account Balance</p>
         <p className="text-4xl font-semibold">£{accountBalance}</p>
       </div>
       <div className="flex justify-around">
-        <div className="bg-gray-700 p-4 rounded-lg flex items-center">
+        <div className="bg-gray-700 p-4 rounded-3xl flex items-center w-2/5">
           <div className="bg-gray-500 p-2 rounded-full mr-3">
             <LuArrowDownSquare />
           </div>
@@ -24,7 +31,7 @@ const Balance: React.FC<BalanceProps> = ({ accountBalance, income, expenses }) =
             <p className="text-xl font-medium">£{income}</p>
           </div>
         </div>
-        <div className="bg-indigo-700 p-4 rounded-lg flex items-center">
+        <div className="bg-indigo-700 p-4 rounded-3xl flex items-center w-2/5">
           <div className="bg-indigo-500 p-2 rounded-full mr-3">
             <LuArrowUpSquare />
           </div>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Balance from '@/components/ui/balance';
 import { useAuthActions } from "@convex-dev/auth/react";
 
 function Home() {
@@ -6,6 +7,7 @@ function Home() {
 
   return (
     <div>
+      <Balance accountBalance={1000} income={500} expenses={200} />
       <h1>Home</h1>
       <Button onClick={signOut}>Sign Out</Button>
     </div>
