008d11ddea73e8eee06c2e5e6e26f0ac57f1c1fb
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2024-09-14 13:52:05 +0100

    card changes

--- src/components/transaction.tsx
+++ src/components/transaction.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import { useTheme } from "../theme-provider";
 interface TransactionProps {
   status: string;
   description: string;
@@ -12,31 +11,45 @@ const Transaction = (props: TransactionProps) => {
     [key: string]: { color: string; text: string };
   } = {
     completed: {
-      color: "bg-green-600",
+      color: "bg-green-400 dark:bg-green-600",
       text: "Completed",
     },
     pending: {
-      color: "bg-blue-500",
+      color: "bg-primary",
       text: "Pending",
     },
     failed: {
-      color: "bg-red-600",
+      color: "bg-red-400 dark:bg-red-600",
       text: "Failed",
     },
   };
+
+  const { theme } = useTheme(); 
+
+  const isDarkMode = theme === "dark";
+
 
   return (
     <div className="px-3 py-2">
       <div className="mt-1 space-y-4">
         {/* Transaction */}
-        <div className="rounded-lg bg-card border border-gray-500 shadow p-4">
+        <div
+          className={`rounded-lg border shadow p-4 ${isDarkMode ? "bg-card border-gray-500" : "bg-glossy border-gray-400"
+            }`}
+        >
           <div className="flex justify-between items-center">
             <div>
-              <p className="text-sm font-bold">{props.description}</p>
-              <p className="text-sm dark:text-gray-400">
+              <p className={`text-sm font-bold ${isDarkMode ? "text-white" : "text-white"}`}>
+                {props.description}
+              </p>
+              <p
+                className={`text-sm ${isDarkMode ? "text-gray-400" : "text-white"}`}
+              >
                 {props.date.toDateString()}{" "}
               </p>
-              <p className="text-sm dark:text-gray-300">
+              <p
+                className={`text-sm ${isDarkMode ? "text-gray-300" : "text-gray-200"}`}
+              >
                 {props.date.toLocaleTimeString([], {
                   hour: "2-digit",
                   minute: "2-digit",
@@ -44,9 +57,12 @@ const Transaction = (props: TransactionProps) => {
               </p>
             </div>
             <div className="text-right">
-              <p className="text-sm font-bold">£{props.amount}</p>
+              <p className={`text-sm font-bold ${isDarkMode ? "text-white" : "text-white"}`}>
+                £{props.amount}
+              </p>
               <div
-                className={`mt-1 rounded-full py-1 px-3 text-xs font-medium text-white ${transactionStatusList[props.status].color}`}
+                className={`mt-1 rounded-full py-1 px-3 text-xs font-medium text-white ${transactionStatusList[props.status].color
+                  }`}
               >
                 {transactionStatusList[props.status].text}
               </div>
